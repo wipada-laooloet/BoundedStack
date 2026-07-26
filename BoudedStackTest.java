b959aadab8dea4722cb5f6e2d8c4a3cccba2676f
sirikornkamk-gaw <sirikorn.kamk@ku.th>
2026-07-26 11:55:10 +0700

    Add files via upload

---
+++ BoudedStackTest.java
@@ -0,0 +1,5 @@
+public class BoudedStackTest {
+    public static void main(String[] args) {
+        
+    }
+}
--- BoundedStack.java
+++ BoundedStack.java
@@ -1,3 +1,32 @@
-public class BoundedStack {
+import java.util.*;
 
+/**
+ * BoundedStack
+ */
+public class BoundedStack {
+    private final List<String> elements ;
+    private final int capacity ;
+    //AF(elements,capacity) = elements คือ Stack ที่ใช้เก็บข้อมูลตามความจุ(capacity) ที่กำหนดไว้
+    //RI
+    //-capacity > 0
+    //-stack ต้องไม่เป็น null
+    //-stack != ""
+    //-จำนวน stack ต้องน้อยกว่าหรือเท่ากับ capacity
+    //
+
+    /**
+     * 
+     * @param capacity
+     */
+    public BoundedStack(int capacity){
+        this.elements = new ArrayList<>();
+        this.capacity = capacity ; 
+    }
+
+    /**
+     * @param s
+     */
+    public void push(String s){
+
+    }
 }
